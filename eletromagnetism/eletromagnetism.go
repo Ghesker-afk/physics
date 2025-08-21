c0c0180250246158b005c97fcb7d0c4c2529cf23
Ghesker-afk <spacexz12345@gmail.com>
2025-08-21 11:31:00 -0300

    feat: added CalculateMagneticForce function.

--- eletromagnetism/eletromagnetism.go
+++ eletromagnetism/eletromagnetism.go
@@ -1,5 +1,7 @@
 package eletromagnetism
 
+import "math"
+
 // Para um corpo interagir com um campo magnético, ele precisa ter carga e estar em movimento (v # 0 e q # 0).
 
 func HasMagneticForce(hasMoviment, hasCarga bool) bool {
@@ -8,4 +10,8 @@ func HasMagneticForce(hasMoviment, hasCarga bool) bool {
 	}
 
 	return false
+}
+
+func CalculateMagneticForce(magneticCamp, electricCharge, velocity, magneticAndVelocityAngle float64) float64 {
+	return magneticCamp * electricCharge * velocity * math.Sin(magneticAndVelocityAngle)
 }
